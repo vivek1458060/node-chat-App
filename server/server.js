055ceba9fdf4fbe2ded9fabaaf471b7868d49e6c
unknown <vivek1458060@gmail.com>
2018-02-04 14:37:46 +0530

    =create generateMessage Utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message.js');
 const publicPath = path.join(__dirname , '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -12,30 +13,16 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to Chat room',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat Room'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'new User joined',
-    createdAt: new Date().getTime()
-  })
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
   socket.on('createMessage', function(message) {
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    console.log('newMessage', message);
 
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    io.emit('newMessage', generateMessage(message.from, message.text));
+
+    //socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
   });
 
   socket.on('disconnect', () => {
---
+++ server/utils/message.test.js
@@ -0,0 +1,16 @@
+var expect = require('expect');
+
+
+var {generateMessage} = require('./message');
+
+ describe('generateMessage', () => {
+   it('should generate correct message Object', () => {
+
+     var from = "Vivek";
+     var text = 'Hii Vivek';
+     var message = generateMessage(from, text);
+
+     expect(message .createdAt).toBeA('number');
+     expect(message).toInclude({from, text});
+   });
+ });
